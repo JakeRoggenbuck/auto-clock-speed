0a83ec1c26dcc15ea9c42f8085e8e29bd4485e58
Cameron <cjpuffnstuff@gmail.com>
2021-07-07 13:16:31 -0700

    Cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Read};
+use std::io::Read;
 use structopt::StructOpt;
 
 enum Error {
@@ -14,25 +14,28 @@ impl From<std::io::Error> for Error {
 }
 
 #[derive(StructOpt)]
-#[structopt(name = "autoclockspeed", about = "Automatic CPU frequency scaler and power saver")]
+#[structopt(
+    name = "autoclockspeed",
+    about = "Automatic CPU frequency scaler and power saver"
+)]
 enum Command {
     #[structopt(name = "get-freq")]
     Get {
-            // Any way to not need to repeat this for each command
-            #[structopt(short, long)]
-            verbose: bool,
-    }
+        // Any way to not need to repeat this for each command
+        #[structopt(short, long)]
+        verbose: bool,
+    },
 }
 
 fn main() {
     match Command::from_args() {
-        Command::Get {verbose} => {
+        Command::Get { verbose } => {
             if verbose {
                 println!("Verbose mode is turned on");
             }
             match get_cpu_freq() {
                 Ok(f) => println!("CPU freq is {} MHz", f),
-                Err(_) => println!("Failed")
+                Err(_) => println!("Failed"),
             }
         }
     }
@@ -45,18 +48,18 @@ fn get_cpu_freq() -> Result<i32, Error> {
     File::open("/proc/cpuinfo")?.read_to_string(&mut cpu_info)?;
 
     // Find all lines that begin with cpu MHz
-    let find_cpu_mhz = cpu_info.split('\n').find(|line|
-        line.starts_with("cpu MHz\t") ||
-        line.starts_with("BogoMIPS") ||
-        line.starts_with("clock\t") ||
-        line.starts_with("bogomips per cpu")
-    );
+    let find_cpu_mhz = cpu_info.split('\n').find(|line| {
+        line.starts_with("cpu MHz\t")
+            || line.starts_with("BogoMIPS")
+            || line.starts_with("clock\t")
+            || line.starts_with("bogomips per cpu")
+    });
 
     // For each line that starts with the clock speed identifier return the number after : as a 32
     // bit integer
-    find_cpu_mhz.and_then(|line| line.split(':').last())
-            .and_then(|val| val.replace("MHz", "").trim().parse::<f64>().ok())
-            .map(|speed| speed as i32)
-            .ok_or(Error::Unknown)
-    
+    find_cpu_mhz
+        .and_then(|line| line.split(':').last())
+        .and_then(|val| val.replace("MHz", "").trim().parse::<f64>().ok())
+        .map(|speed| speed as i32)
+        .ok_or(Error::Unknown)
 }

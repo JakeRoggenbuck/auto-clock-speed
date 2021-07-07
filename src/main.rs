755147ed46606da1759b9050b42b8573452ea7a6
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-07-07 14:37:05 -0700

    Add check for turbo

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use structopt::StructOpt;
 use error::Error;
-use system::get_cpu_freq;
+use structopt::StructOpt;
+use system::{check_cpu_freq, check_turbo_enabled};
 
 pub mod error;
 pub mod system;
-
 
 #[derive(StructOpt)]
 #[structopt(
@@ -13,23 +12,28 @@ pub mod system;
 )]
 enum Command {
     #[structopt(name = "get-freq")]
-    Get {
-        // Any way to not need to repeat this for each command
-        #[structopt(short, long)]
-        verbose: bool,
-    },
+    GetFreq,
+
+    #[structopt(name = "get-turbo")]
+    GetTurbo,
 }
 
 fn main() {
     match Command::from_args() {
-        Command::Get { verbose } => {
-            if verbose {
-                println!("Verbose mode is turned on");
-            }
-            match get_cpu_freq() {
-                Ok(f) => println!("CPU freq is {} MHz", f),
-                Err(_) => println!("Failed"),
-            }
-        }
+        Command::GetFreq {} => match check_cpu_freq() {
+            Ok(f) => println!("CPU freq is {} MHz", f),
+            Err(_) => println!("Failed"),
+        },
+        Command::GetTurbo {} => match check_turbo_enabled() {
+            Ok(a) => println!(
+                "{}",
+                if a {
+                    "Turbo is enabled"
+                } else {
+                    "Turbo is not enabled"
+                }
+            ),
+            Err(_) => println!("Failed"),
+        },
     }
 }
--- src/system.rs
+++ src/system.rs
@@ -1,10 +1,10 @@
+use super::Error;
 use std::fs::File;
 use std::io::Read;
-use super::Error;
 
 // https://docs.rs/sys-info/0.7.0/src/sys_info/lib.rs.html#367-406
-pub fn get_cpu_freq() -> Result<i32, Error> {
-    let mut cpu_info = String::new();
+pub fn check_cpu_freq() -> Result<i32, Error> {
+    let mut cpu_info: String = String::new();
     File::open("/proc/cpuinfo")?.read_to_string(&mut cpu_info)?;
 
     // Find all lines that begin with cpu MHz
@@ -22,4 +22,19 @@ pub fn get_cpu_freq() -> Result<i32, Error> {
         .and_then(|val| val.replace("MHz", "").trim().parse::<f64>().ok())
         .map(|speed| speed as i32)
         .ok_or(Error::Unknown)
+}
+
+pub fn check_turbo_enabled() -> Result<bool, Error> {
+    let mut is_turbo: String = String::new();
+    let turbo_path: &str = "/sys/devices/system/cpu/intel_pstate/no_turbo";
+    File::open(turbo_path)?.read_to_string(&mut is_turbo)?;
+
+    // Remove the last character (the newline)
+    is_turbo.pop();
+    // The file will be something like 0 or 1, parse this into an int
+    match is_turbo.parse::<i8>() {
+        Err(e) => panic!("{}", e),
+        // Zero means turbo is enabled, so return true
+        Ok(a) => Ok(a == 0),
+    }
 }

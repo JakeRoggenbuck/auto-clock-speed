7a2c8090726ce64031fce8d5ffa0fe008a32b225
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-07-07 23:57:46 -0700

    Add implemented traits

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use super::system::get_some_cpu_int_by_path;
+
 trait Speed {
     fn set_max(&mut self, max: i32);
     fn set_min(&mut self, min: i32);
@@ -13,4 +15,43 @@ struct CPU {
     cur_freq: i32,
     base_freq: i32,
     turbo: bool,
+}
+
+impl Speed for CPU {
+    fn set_max(&mut self, max: i32) {
+        // TODO: change the file with the speed
+        self.max_freq = max;
+    }
+
+    fn set_min(&mut self, min: i32) {
+        // TODO: change the file with the speed
+        self.min_freq = min;
+    }
+
+    fn get_max(&mut self) {
+        match get_some_cpu_int_by_path(self.name.clone(), "cpufreq/scaling_max_freq".to_string()) {
+            Ok(a) => {
+                self.max_freq = a;
+            }
+            Err(_) => eprint!("Failed"),
+        }
+    }
+
+    fn get_min(&mut self) {
+        match get_some_cpu_int_by_path(self.name.clone(), "cpufreq/scaling_min_freq".to_string()) {
+            Ok(a) => {
+                self.min_freq = a;
+            }
+            Err(_) => eprint!("Failed"),
+        }
+    }
+
+    fn get_cur(&mut self) {
+        match get_some_cpu_int_by_path(self.name.clone(), "cpufreq/scaling_cur_freq".to_string()) {
+            Ok(a) => {
+                self.cur_freq = a;
+            }
+            Err(_) => eprint!("Failed"),
+        }
+    }
 }

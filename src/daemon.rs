feef54bdd4ef172f6e28e7db0af43e9198863b9a
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-07-08 01:25:24 -0700

    Add comments

--- src/daemon.rs
+++ src/daemon.rs
@@ -19,7 +19,9 @@ impl Checker for Daemon {
         let five_seconds = time::Duration::from_secs(5);
 
         loop {
+            // Update all the speed from the cpus before they may get displayed or used
             self.update_all();
+
             if self.verbose {
                 self.print();
             }
@@ -28,21 +30,26 @@ impl Checker for Daemon {
         }
     }
 
+    /// Output the values from each cpu
     fn print(&mut self) {
         for cpu in &self.cpus {
             println!("{:?}", cpu);
         }
     }
 
-    fn update_all(&mut self) { }
+    fn update_all(&mut self) {
+        // TODO: find a way to go through self.cpus and run update() on each one
+    }
 }
 
 pub fn daemon_init(verbose: bool) -> Result<Daemon, Error> {
+    // Create a new Daemon
     let mut daemon: Daemon = Daemon {
         cpus: Vec::<CPU>::new(),
         verbose,
     };
 
+    // Make a cpu struct for each cpu listed
     for cpu in list_cpus()? {
         let mut new = CPU {
             name: cpu,
@@ -50,6 +57,7 @@ pub fn daemon_init(verbose: bool) -> Result<Daemon, Error> {
             min_freq: 0,
             cur_freq: 0,
         };
+        // Fill that value that were zero with real values
         new.init_cpu();
         daemon.cpus.push(new);
     }

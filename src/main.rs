9c12989dac30d6d0b117d25c1de3726e669aa78c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-09-07 00:00:50 -0700

    Add sub sub commands for get

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use daemon::{daemon_init, Checker};
 use display::{
-    print_available_governors, print_cpu_governors, print_cpu_speeds, print_cpu_temp, print_cpus, print_freq,
-    print_turbo,
+    print_available_governors, print_cpu_governors, print_cpu_speeds, print_cpu_temp, print_cpus,
+    print_freq, print_turbo,
 };
-use error::{Error, GovGetError, TempGetError, GovSetError, SpeedGetError, SpeedSetError};
+use error::{Error, GovGetError, GovSetError, SpeedGetError, SpeedSetError, TempGetError};
 use power::{read_battery_charge, read_lid_state, read_power_source};
 use std::process::exit;
 use structopt::StructOpt;
@@ -20,58 +20,70 @@ pub mod power;
 pub mod system;
 
 #[derive(StructOpt)]
-#[structopt(
-    name = "autoclockspeed",
-    about = "Automatic CPU frequency scaler and power saver"
-)]
-enum Command {
+enum GetType {
+    /// Get the power
+    #[structopt(name = "power")]
+    Power,
+
     /// The overall frequency of your cpu
-    #[structopt(name = "get-freq")]
-    GetFreq {
+    #[structopt(name = "freq")]
+    Freq {
         #[structopt(short, long)]
         raw: bool,
     },
 
-    #[structopt(name = "power")]
-    Power,
-
     /// Get whether turbo is enabled or not
-    #[structopt(name = "get-turbo")]
-    GetTurbo {
+    #[structopt(name = "turbo")]
+    Turbo {
         #[structopt(short, long)]
         raw: bool,
     },
 
     /// Get the available governor
-    #[structopt(name = "get-available-governors")]
-    GetAvailableGovernors {
+    #[structopt(name = "available-govs")]
+    AvailableGovs {
         #[structopt(short, long)]
         raw: bool,
     },
 
     /// The names of the core
-    #[structopt(name = "get-cpus")]
-    GetCPUS,
+    #[structopt(name = "cpus")]
+    CPUS,
 
     /// The speed of the individual cores
-    #[structopt(name = "get-cpu-speeds")]
-    GetSpeeds {
+    #[structopt(name = "cpu-speeds")]
+    Speeds {
         #[structopt(short, long)]
         raw: bool,
     },
 
     /// The temperature of the individual cores
-    #[structopt(name = "get-cpu-temp")]
-    GetTemp {
+    #[structopt(name = "cpu-temp")]
+    Temp {
         #[structopt(short, long)]
         raw: bool,
     },
 
     /// The governors of the individual cores
-    #[structopt(name = "get-cpu-governors")]
-    GetGovernors {
+    #[structopt(name = "cpu-govs")]
+    Govs {
         #[structopt(short, long)]
         raw: bool,
+    },
+}
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "autoclockspeed",
+    about = "Automatic CPU frequency scaler and power saver"
+)]
+enum Command {
+    /// Get a specific value or status
+    #[structopt(name = "get")]
+    Get {
+        /// The type of value to request
+        #[structopt(subcommand)]
+        get: GetType,
     },
 
     /// Run the daemon, this checks and edit your cpu's speed
@@ -97,48 +109,50 @@ enum Command {
 
 fn main() {
     match Command::from_args() {
-        Command::GetFreq { raw } => match check_cpu_freq() {
-            Ok(f) => print_freq(f, raw),
-            Err(_) => eprintln!("Faild to get cpu frequency"),
-        },
-        Command::Power {} => match read_lid_state() {
-            Ok(f) => match read_battery_charge() {
-                Ok(c) => match read_power_source() {
-                    Ok(p) => {
-                        println!("Lid: {} Bat: {} Plugged in: {}", f, c, p)
-                    }
-                    Err(_) => eprintln!("Faild to get read power source"),
-                },
-                Err(_) => eprintln!("Faild to get read battery charger"),
+        Command::Get { get } => match get {
+            GetType::Freq { raw } => match check_cpu_freq() {
+                Ok(f) => print_freq(f, raw),
+                Err(_) => eprintln!("Faild to get cpu frequency"),
             },
-            Err(_) => eprintln!("Faild to get read lid state"),
-        },
-        Command::GetTurbo { raw } => match check_turbo_enabled() {
-            Ok(turbo_enabled) => print_turbo(turbo_enabled, raw),
-            Err(_) => println!("Failed to get turbo status"),
-        },
-        Command::GetAvailableGovernors { raw } => match check_available_governors() {
-            Ok(available_governors) => print_available_governors(available_governors, raw),
-            Err(_) => println!("Failed to get available governors"),
-        },
-        Command::GetCPUS {} => match list_cpus() {
-            Ok(cpus) => match check_cpu_name() {
-                Ok(name) => print_cpus(cpus, name),
+            GetType::Power {} => match read_lid_state() {
+                Ok(f) => match read_battery_charge() {
+                    Ok(c) => match read_power_source() {
+                        Ok(p) => {
+                            println!("Lid: {} Bat: {} Plugged in: {}", f, c, p)
+                        }
+                        Err(_) => eprintln!("Faild to get read power source"),
+                    },
+                    Err(_) => eprintln!("Faild to get read battery charger"),
+                },
+                Err(_) => eprintln!("Faild to get read lid state"),
+            },
+            GetType::Turbo { raw } => match check_turbo_enabled() {
+                Ok(turbo_enabled) => print_turbo(turbo_enabled, raw),
+                Err(_) => println!("Failed to get turbo status"),
+            },
+            GetType::AvailableGovs { raw } => match check_available_governors() {
+                Ok(available_governors) => print_available_governors(available_governors, raw),
+                Err(_) => println!("Failed to get available governors"),
+            },
+            GetType::CPUS {} => match list_cpus() {
+                Ok(cpus) => match check_cpu_name() {
+                    Ok(name) => print_cpus(cpus, name),
+                    Err(_) => println!("Failed get list of cpus"),
+                },
                 Err(_) => println!("Failed get list of cpus"),
             },
-            Err(_) => println!("Failed get list of cpus"),
-        },
-        Command::GetSpeeds { raw } => match list_cpu_speeds() {
-            Ok(cpu_speeds) => print_cpu_speeds(cpu_speeds, raw),
-            Err(_) => println!("Failed to get list of cpu speeds"),
-        },
-        Command::GetTemp { raw } => match list_cpu_temp() {
-            Ok(cpu_temp) => print_cpu_temp(cpu_temp, raw),
-            Err(_) => println!("Failed to get list of cpu temperature"),
-        },
-        Command::GetGovernors { raw } => match list_cpu_governors() {
-            Ok(cpu_governors) => print_cpu_governors(cpu_governors, raw),
-            Err(_) => println!("Failed to get list of cpu governors"),
+            GetType::Speeds { raw } => match list_cpu_speeds() {
+                Ok(cpu_speeds) => print_cpu_speeds(cpu_speeds, raw),
+                Err(_) => println!("Failed to get list of cpu speeds"),
+            },
+            GetType::Temp { raw } => match list_cpu_temp() {
+                Ok(cpu_temp) => print_cpu_temp(cpu_temp, raw),
+                Err(_) => println!("Failed to get list of cpu temperature"),
+            },
+            GetType::Govs { raw } => match list_cpu_governors() {
+                Ok(cpu_governors) => print_cpu_governors(cpu_governors, raw),
+                Err(_) => println!("Failed to get list of cpu governors"),
+            },
         },
         Command::Run { quiet, delay } => match daemon_init(!quiet, delay, true) {
             Ok(mut d) => {

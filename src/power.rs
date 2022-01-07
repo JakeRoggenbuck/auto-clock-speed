40fe18f0e3cd9999caeff211307668596ca02e40
Camerooooon <cjpuffnstuff@gmail.com>
2022-01-07 08:24:05 -0800

    Add the rest of the power status backup strings

--- src/power.rs
+++ src/power.rs
@@ -69,13 +69,28 @@ pub fn read_lid_state() -> Result<LidState, Error> {
 }
 
 pub fn read_battery_charge() -> Result<i8, Error> {
-    if !Path::new("/sys/class/power_supply/BAT0/capacity").exists() {
-        // If the power source does not exist, then it's plugged in, so 100%
-        return Ok(100);
-    }
+    let battery_charge_path: Vec<&str> = vec![
+        "/sys/class/power_supply/BAT/capacity",
+        "/sys/class/power_supply/BAT0/capacity",
+        "/sys/class/power_supply/BAT1/capacity"
+    ];
+
+    let path: &str = match get_best_path(battery_charge_path) {
+        Ok(path) => {
+            path
+        },
+        Err(error) => {
+            if error.type_id() == Error::IO.type_id() {
+                // Make sure to return IO error if one occurs
+                return Err(error);
+            }
+            // If it doesn't exist then it is plugged in so make it 100% percent capacity
+            return Ok(100);
+        }
+    };
 
     let mut cap_str: String = String::new();
-    File::open("/sys/class/power_supply/BAT0/capacity")?.read_to_string(&mut cap_str)?;
+    File::open(path)?.read_to_string(&mut cap_str)?;
 
     // Remove the \n char
     cap_str.pop();
@@ -84,13 +99,30 @@ pub fn read_battery_charge() -> Result<i8, Error> {
 }
 
 pub fn read_power_source() -> Result<bool, Error> {
-    if !Path::new("/sys/class/power_supply/AC/online").exists() {
-        println!("Unexpected, the directory /sys/class/power_supply/AC/online doesn't exist? Do you not have a power source?");
-        return Ok(true);
-    }
+
+    let power_source_path: Vec<&str> = vec![ 
+        "/sys/class/power_supply/AC/online",
+        "/sys/class/power_supply/AC0/online",
+        "/sys/class/power_supply/AC1/online",
+        "/sys/class/power_supply/ACAD/online"
+    ];
+
+    let path: &str = match get_best_path(power_source_path) {
+        Ok(path) => {
+            path
+        },
+        Err(error) => {
+            if error.type_id() == Error::IO.type_id() {
+                // Make sure to return IO error if one occurs
+                return Err(error);
+            }
+            println!("Unexpected, the directory /sys/class/power_supply/AC/online doesn't exist? Do you not have a power source?");
+            return Ok(true);
+        }
+    };
 
     let mut pwr_str: String = String::new();
-    File::open("/sys/class/power_supply/AC/online")?.read_to_string(&mut pwr_str)?;
+    File::open(path)?.read_to_string(&mut pwr_str)?;
 
     // Remove the \n char
     pwr_str.pop();

18d65548400d7e58ad9acac289aaa489b07fb97d
Camerooooon <cjpuffnstuff@gmail.com>
2022-06-19 20:58:36 -0700

    Only list thermal zones not cooling devices

--- src/thermal.rs
+++ src/thermal.rs
@@ -1,5 +1,7 @@
 use std::fs::{read_dir};
 
+const THERMAL_ZONE_DIR: &str = "/sys/class/thermal/";
+
 pub struct ThermalZone {
     pub name: String,
     pub location: String,
@@ -21,8 +23,11 @@ impl Default for ThermalZone {
 pub fn read_thermal_zones() -> Vec::<ThermalZone> {
     let mut zones = Vec::<ThermalZone>::new();
 
-    for a in read_dir("/sys/class/thermal/").expect("Could not read thermal directory") {
-        let path_string: String = format!("{:?}", a.unwrap().path());
+    for a in read_dir(THERMAL_ZONE_DIR).expect("Could not read thermal directory") {
+        let path_string: String = format!("{}", a.unwrap().path().to_string_lossy());
+        if !path_string.starts_with(&[THERMAL_ZONE_DIR, "thermal_zone"].concat()) {
+            continue;
+        }
         println!("{}", path_string);
 
     }

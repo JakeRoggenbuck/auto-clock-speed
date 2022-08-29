dddee2eb27eef88be6766b069deaaf37ab48e993
Frederic Brin <duckx@mezimail.com>
2022-08-29 12:45:02 +0200

    Remove get_sysfs_path_by_glob from power
    As it is now hosted in sysfs module

--- src/power.rs
+++ src/power.rs
@@ -1,7 +1,6 @@
-use globset::Glob;
 use std::any::Any;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use super::create_issue;
 use super::Error;
@@ -15,24 +14,6 @@ const POWER_SOURCE_PATH: [&str; 4] = [
     "/sys/class/power_supply/AC1/online",
     "/sys/class/power_supply/ACAD/online",
 ];
-
-// Lookup a Hdw based on its Parent Path & a glob
-pub fn get_sysfs_path_by_glob(sysfs_parent_path: &str, hdw_glob: &str) -> Result<PathBuf, Error> {
-    let mut glob_path = sysfs_parent_path.to_string();
-    glob_path.push_str(hdw_glob);
-
-    let glob = Glob::new(&glob_path).unwrap().compile_matcher();
-    let entries = fs::read_dir(sysfs_parent_path).unwrap();
-
-    for entry in entries {
-        let entry = entry.unwrap();
-        let pathbuf = entry.path();
-        if glob.is_match(&pathbuf) {
-            return Ok(pathbuf);
-        }
-    }
-    Err(Error::HdwNotFound)
-}
 
 pub fn get_best_path(paths: [&'static str; 4]) -> Result<&str, Error> {
     for path in paths.iter() {

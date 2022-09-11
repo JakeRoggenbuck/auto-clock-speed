6bb45844e00076957b2dc992bc4ed942848d3295
Camerooooon <cjpuffnstuff@gmail.com>
2022-09-10 19:35:05 -0700

    Reduce even more unwraps in sysfs

--- src/sysfs.rs
+++ src/sysfs.rs
@@ -27,11 +27,13 @@ pub fn get_path_by_glob(sysfs_parent_path: &str, hdw_glob: &str) -> Result<PathB
     let mut glob_path = sysfs_parent_path.to_string();
     glob_path.push_str(hdw_glob);
 
-    let glob = Glob::new(&glob_path).unwrap().compile_matcher();
-    let entries = fs::read_dir(sysfs_parent_path).unwrap();
+    let glob = Glob::new(&glob_path)
+        .map_err(|_| Error::Unknown)?
+        .compile_matcher();
+    let entries = fs::read_dir(sysfs_parent_path).map_err(|_| Error::Unknown)?;
 
     for entry in entries {
-        let entry = entry.unwrap();
+        let entry = entry.map_err(|_| Error::Unknown)?;
         let pathbuf = entry.path();
         if glob.is_match(&pathbuf) {
             return Ok(pathbuf);

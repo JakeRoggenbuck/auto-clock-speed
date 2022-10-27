dfec2331447c779ac35dadb7861d975c711dc7c8
Shuzhengz <treez.zhang@gmail.com>
2022-10-26 18:41:34 -0700

    fmt

--- src/setup.rs
+++ src/setup.rs
@@ -1,12 +1,14 @@
-use crate::system::inside_wsl;
 use super::system::inside_docker;
 use super::warn_user;
+use crate::system::inside_wsl;
 
 pub fn setup() {
     if inside_wsl() {
-        warn_user!("ACS is intended to run on an actual linux distribution, \
+        warn_user!(
+            "ACS is intended to run on an actual linux distribution, \
         the program will NOT work inside of Windows Subsystem for Linux \
-        Please install an actual distribution of Linux");
+        Please install an actual distribution of Linux"
+        );
     }
     if inside_docker() {
         warn_user!("Stats may be incorrect if running inside docker.");

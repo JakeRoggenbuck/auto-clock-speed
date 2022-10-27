73053b43277e6de329d85fddc85cece0068ba4f3
Shuzhengz <treez.zhang@gmail.com>
2022-10-26 18:29:55 -0700

    simplify

--- src/system.rs
+++ src/system.rs
@@ -48,20 +48,14 @@ pub fn inside_docker() -> bool {
 
 /// Detects if being executed inside of windows subsystem for linux
 pub fn inside_wsl() -> bool {
-    if let Ok(path_osrelease) = std::fs::read("/proc/sys/kernel/osrelease") {
-        return uft8_to_str(&path_osrelease).contains("microsoft") || uft8_to_str(&path_osrelease).contains("wsl");
-    } else if let Ok(path_version) = std::fs::read("/proc/version") {
-        return uft8_to_str(&path_version).contains("microsoft") || uft8_to_str(&path_version).contains("wsl");
+    if Path::new("/proc/sys/kernel/osrelease").exists() {
+        let o = fs::read_to_string("/proc/sys/kernel/osrelease").expect("Unable to read file");
+        return o.contains("microsoft");
+    } else if Path::new("/proc/version").exists() {
+        let v = fs::read_to_string("/proc/version").expect("Unable to read file");
+        return v.contains("microsoft") || v.contains("WSL");
     }
     false
-}
-
-fn uft8_to_str(path: &[u8]) -> String {
-    return if let Ok(st) = std::str::from_utf8(&path) {
-        st.to_ascii_lowercase()
-    } else {
-        "a".to_string()
-    }
 }
 
 fn open_cpu_info() -> Result<String, Error> {

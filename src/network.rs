81aa215c8790e5874abb73dd973d2dcc31689f7e
Camerooooon <cjpuffnstuff@gmail.com>
2022-11-28 21:15:54 -0800

    Switch to serde for data serialization

--- src/network.rs
+++ src/network.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 use crate::logger::Log;
 
 use super::daemon::Daemon;
@@ -11,13 +13,12 @@ use std::os::unix::net::UnixListener;
 use std::str::ParseBoolError;
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::time::SystemTime;
 
 pub mod hook;
 pub mod listen;
 pub mod send;
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Serialize, Deserialize)]
 pub enum Packet {
     Hello(String),
     HelloResponse(String, u32),
@@ -55,84 +56,16 @@ impl From<ParseBoolError> for PacketParseError {
 }
 
 pub fn parse_packet(packet: &str) -> Result<Packet, PacketParseError> {
-    let mut packet_split = packet.split('|');
-    let packet_type = packet_split.next().ok_or(PacketParseError)?;
-    match packet_type {
-        "0" => Ok(Packet::Hello(
-            packet_split.next().ok_or(PacketParseError)?.to_string(),
-        )),
-        "1" => Ok(Packet::HelloResponse(
-            packet_split.next().ok_or(PacketParseError)?.to_string(),
-            packet_split
-                .next()
-                .ok_or(PacketParseError)?
-                .parse::<u32>()?,
-        )),
-        "2" => Ok(Packet::DaemonDisableRequest()),
-        "3" => Ok(Packet::DaemonDisableResponse(
-            packet_split
-                .next()
-                .ok_or(PacketParseError)?
-                .parse::<bool>()?,
-        )),
-        "4" => Ok(Packet::DaemonEnableRequest()),
-        "5" => Ok(Packet::DaemonEnableResponse(
-            packet_split
-                .next()
-                .ok_or(PacketParseError)?
-                .parse::<bool>()?,
-        )),
-        "6" => Ok(Packet::DaemonStatusRequest()),
-        "7" => Ok(Packet::DaemonStatusResponse(
-            packet_split
-                .next()
-                .ok_or(PacketParseError)?
-                .parse::<bool>()?,
-        )),
-        "8" => Ok(Packet::DaemonLogRequest()),
-        "9" => Ok(Packet::DaemonLogResponse(
-            packet_split
-                .map(|x| {
-                    serde_json::from_str(x).unwrap_or(Log {
-                        message: "?".to_string(),
-                        severity: logger::Severity::Error,
-                        timestamp: SystemTime::now(),
-                    })
-                })
-                .collect(),
-        )),
-        "10" => Ok(Packet::DaemonLogEvent(
-            serde_json::from_str(packet_split.next().ok_or(PacketParseError)?).unwrap(),
-        )),
-        _ => Err(PacketParseError),
-    }
+    serde_json::from_str(packet).map_err(|_| PacketParseError)
 }
 
 impl Display for Packet {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Packet::Hello(data) => writeln!(f, "0|{}", data),
-            Packet::HelloResponse(data, version) => writeln!(f, "1|{}|{}", data, version),
-            Packet::DaemonDisableRequest() => writeln!(f, "2"),
-            Packet::DaemonDisableResponse(data) => writeln!(f, "3|{}", data),
-            Packet::DaemonEnableRequest() => writeln!(f, "4"),
-            Packet::DaemonEnableResponse(data) => writeln!(f, "5|{}", data),
-            Packet::DaemonStatusRequest() => writeln!(f, "6"),
-            Packet::DaemonStatusResponse(data) => writeln!(f, "7|{}", data),
-            Packet::DaemonLogRequest() => writeln!(f, "8"),
-            Packet::DaemonLogResponse(data) => {
-                writeln!(
-                    f,
-                    "9|{}",
-                    data.iter()
-                        .map(|x| { serde_json::to_string(x).unwrap_or("?".to_string()) })
-                        .collect::<String>()
-                )
-            }
-
-            Packet::Unknown => writeln!(f),
-            Packet::DaemonLogEvent(_) => todo!(),
-        }
+        writeln!(
+            f,
+            "{}",
+            serde_json::to_string(self).unwrap_or_else(|_| "?".to_string())
+        )
     }
 }
 

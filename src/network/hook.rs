21fc7f6baa89c2c2858cd23fffd7351e1a428011
Camerooooon <cjpuffnstuff@gmail.com>
2022-11-29 22:44:56 -0800

    Remove function

--- src/network/hook.rs
+++ src/network/hook.rs
@@ -38,14 +38,49 @@ pub fn hook(path: &'static str, c_daemon_mutex: Arc<Mutex<Daemon>>) {
                     logger::Origin::Client,
                 );
                 let daemon = &mut c_daemon_mutex.lock().unwrap();
-                match restore_logs(&mut stream, &mut daemon.logger.logs) {
+                let packet = Packet::DaemonLogRequest();
+                write_packet!(stream, packet);
+
+                let mut reader = BufReader::new(&stream);
+                let mut line = String::new();
+
+                match reader.read_line(&mut line) {
                     Ok(_) => {}
-                    Err(e) => log_to_daemon_origin(
-                        &c_daemon_mutex,
-                        &format!("Failed to restore logs: {:?}", e),
-                        logger::Severity::Error,
-                        logger::Origin::Client,
-                    ),
+                    Err(_) => {
+                        log_to_daemon_origin(
+                            &c_daemon_mutex,
+                            "Faied to read response from daemon",
+                            logger::Severity::Error,
+                            logger::Origin::Client,
+                        );
+                    }
+                };
+
+                match parse_packet(&line) {
+                    Ok(p) => match p {
+                        Packet::DaemonLogResponse(new_logs) => {
+                            for mut log in new_logs {
+                                log.origin = Origin::Daemon;
+                                daemon.logger.logs.push(log)
+                            }
+                        }
+                        _ => {
+                            log_to_daemon_origin(
+                                &c_daemon_mutex,
+                                "Unexpected response packet from daemon? Incorrect protocol version?",
+                                logger::Severity::Error,
+                                logger::Origin::Client,
+                            );
+                        }
+                    },
+                    Err(_) => {
+                        log_to_daemon_origin(
+                            &c_daemon_mutex,
+                            "Failed to parse packet from daemon? Incorrect protocol version?",
+                            logger::Severity::Error,
+                            logger::Origin::Client,
+                        );
+                    }
                 }
             }
             Err(e) => {
@@ -61,31 +96,6 @@ pub fn hook(path: &'static str, c_daemon_mutex: Arc<Mutex<Daemon>>) {
                 return;
             }
         }
-
         stream.shutdown(std::net::Shutdown::Both).unwrap();
     });
-}
-
-fn restore_logs(stream: &mut UnixStream, logs: &mut Vec<Log>) -> Result<(), Error> {
-    let packet = Packet::DaemonLogRequest();
-    write_packet!(stream, packet);
-
-    let mut reader = BufReader::new(stream);
-    let mut line = String::new();
-
-    reader.read_line(&mut line)?;
-
-    match parse_packet(&line) {
-        Ok(p) => match p {
-            Packet::DaemonLogResponse(new_logs) => {
-                for mut log in new_logs {
-                    log.origin = Origin::Daemon;
-                    logs.push(log)
-                }
-                Ok(())
-            }
-            _ => Err(Error::Parse),
-        },
-        Err(_) => Err(Error::Parse),
-    }
 }

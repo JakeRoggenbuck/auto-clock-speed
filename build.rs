2a1d10b3ce7745fadae2abf53c08fdf3fb1aebf3
Jake <jakeroggenbuck2@gmail.com>
2023-06-16 15:33:47 -0700

    Add comments

--- build.rs
+++ build.rs
@@ -3,11 +3,13 @@
 use std::process::Command;
 
 fn main() {
+    // Get commit hash of last commit
     let output = Command::new("git")
         .args(["rev-parse", "HEAD"])
         .output()
         .unwrap();
 
     let git_hash = String::from_utf8(output.stdout).unwrap();
+    // Set commit hash to env to read later with --commit option
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }

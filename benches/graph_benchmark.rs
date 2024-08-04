fa0ef0035506e836189dc0e9d97942382252387e
Jake <jakeroggenbuck2@gmail.com>
2024-08-04 14:15:01 -0700

    Add clippy fixes

--- benches/graph_benchmark.rs
+++ benches/graph_benchmark.rs
@@ -20,7 +20,10 @@ fn clear_before_benchmark(c: &mut Criterion) {
     let graph = Graph::new();
     let mut vec = vec![0.0; 100];
     c.bench_function("clear_before", |b| {
-        b.iter(|| black_box(graph.clear_before(&mut vec)))
+        b.iter(|| {
+            graph.clear_before(&mut vec);
+            black_box(())
+        })
     });
 }
 

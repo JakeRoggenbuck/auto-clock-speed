03ed8d1ff4dafc20daf85bfd31502762d3496158
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-01-19 00:14:13 -0800

    Add comments for each of the settings

--- src/settings.rs
+++ src/settings.rs
@@ -9,17 +9,29 @@ pub trait DefaultTesting {
 #[derive(Clone, Debug)]
 pub struct Settings {
     pub verbose: bool,
+    /// The delay between rerenders when connected to a power source
     pub delay: u64,
+    /// The delay between rerenders when using battery power only
     pub delay_battery: u64,
+    /// If the daemon should edit the governor based on the rules
     pub edit: bool,
+    /// If the daemon should hook on to another client daemon
     pub hook: bool,
+    /// If ACS should show an ASCII animation when turbo is enabled
     pub animation: bool,
+    /// The type of graph that should be displayed with ACS monit
     pub graph: GraphType,
+    /// If it should show the latest commit (used for debugging mostly)
     pub commit: bool,
+    /// If the daemon is running in testing mode
     pub testing: bool,
+    /// The output csv filepath
     pub csv_file: String,
+    /// If ACS should log to a file
     pub log_csv: bool,
+    /// The size in MB that should be written before the file gets overwritten
     pub log_size_cutoff: i32,
+    /// If ACS should show the settings and exit (used for debugging)
     pub show_settings: bool,
 }
 
